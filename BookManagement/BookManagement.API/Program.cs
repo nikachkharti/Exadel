e8ebdc1d8c1405bd619c05de1996eaa71568e4aa
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-02-21 04:10:36 +0400

    API project initialized with minimum startup requirements

---
+++ BookManagement/BookManagement.API/Program.cs
@@ -0,0 +1,25 @@
+using BookManagement.API.Extensions;
+
+namespace BookManagement.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.AddControllers();
+            builder.AddEndpointsApiExplorer();
+            builder.AddSwagger();
+
+            var app = builder.Build();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
