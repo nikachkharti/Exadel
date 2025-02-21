e2d69585509efd53ada25c86a06055672d952d6e
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-02-21 04:21:37 +0400

    Entities added

---
+++ BookManagement/BookManagement.Models/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookManagement.Models.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public DateTime PublicationYear { get; set; }
+        public int ViewCount { get; set; } = 0;
+    }
+}
