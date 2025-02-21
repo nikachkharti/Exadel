9593ab8ff659c66081c8a6d6fc455ed609f7bd26
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-02-21 17:22:05 +0400

    Popularity score computed

--- BookManagement/BookManagement.Models/Dtos/BookForGettingDto.cs
+++ BookManagement/BookManagement.Models/Dtos/BookForGettingDto.cs
@@ -8,5 +8,6 @@
         public DateTime PublicationYear { get; set; }
         public int ViewCount { get; set; }
         public int Age { get; set; }
+        public double PopularityScore { get; set; }
     }
 }
