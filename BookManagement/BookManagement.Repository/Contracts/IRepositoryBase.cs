8dee0b9fc39bf76ea5fe1075f5966191de8a1354
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-02-21 04:39:16 +0400

    Generic repository implemented for database communication

---
+++ BookManagement/BookManagement.Repository/Contracts/IRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace BookManagement.Repository.Contracts
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter, int pageNumber, int pageSize, string includeProperties = null);
+        Task<List<T>> GetAllAsync(int pageNumber, int pageSize, string includeProperties = null);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter, string includeProperties = null);
+        Task<List<T>> GetAllAsync(string includeProperties = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter, string includeProperties = null);
+        Task AddAsync(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ BookManagement/BookManagement.Repository/Implementations/RepositoryBase.cs
@@ -0,0 +1,117 @@
+﻿using BookManagement.Repository.Contracts;
+using BookManagement.Repository.Data.EF;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BookManagement.Repository.Implementations
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        private readonly DbSet<T> _dbSet;
+        public RepositoryBase(ApplicationDbContext context)
+        {
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter, int pageNumber, int pageSize, string includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (!string.IsNullOrWhiteSpace(includeProperties))
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query
+                        .Include(includeProperty)
+                        .Skip((pageNumber - 1) * pageSize)
+                        .Take(pageSize);
+                }
+            }
+
+            var entities = await query
+                .Where(filter)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return entities;
+        }
+        public async Task<List<T>> GetAllAsync(int pageNumber, int pageSize, string includeProperties = null)
+        {
+            if (!string.IsNullOrWhiteSpace(includeProperties))
+            {
+                IQueryable<T> query = _dbSet;
+
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+
+                return await
+                    query
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync();
+            }
+
+            return await
+                _dbSet
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter, string includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (!string.IsNullOrWhiteSpace(includeProperties))
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            var entities = await query
+                .Where(filter)
+                .ToListAsync();
+
+            return entities;
+        }
+        public async Task<List<T>> GetAllAsync(string includeProperties = null)
+        {
+            if (!string.IsNullOrWhiteSpace(includeProperties))
+            {
+                IQueryable<T> query = _dbSet;
+
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+
+                return await query.ToListAsync();
+            }
+
+            return await _dbSet.ToListAsync();
+        }
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter, string includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            var entity = await query.FirstOrDefaultAsync(filter);
+
+            return entity;
+        }
+        public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+        public void Remove(T entity) => _dbSet.Remove(entity);
+        public void RemoveRange(IEnumerable<T> entities) => _dbSet.RemoveRange(entities);
+    }
+}
